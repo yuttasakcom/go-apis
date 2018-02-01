eabc35713954e0dbecdb3b3562fe97774a722e63
YoProgrammer <yuttasakcom@gmail.com>
2018-02-01 11:12:11 +0700

    update readme

--- main.go
+++ main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/yuttasakcom/go-apis/routes"
 )
 
 func main() {
-	r := routes.Router()
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
 
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	r := routes.Router()
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
 	log.Println("go-apis running at port:" + port)
 	log.Println("Press CTRL-C to stop")
-	log.Fatal(http.ListenAndServe(":"+port, r))
+
+	killSignal := <-stop
+	switch killSignal {
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Got SIGTERM...")
+	}
+
+	log.Println("The service is shutting down...")
+	srv.Shutdown(context.Background())
+	log.Println("Server gracefully stopped")
 }
